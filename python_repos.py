515a114e2293dffb72ca784f17c8679a43ec64b2
dipsonu10 <65275144+dipsonu10@users.noreply.github.com>
2020-05-13 18:16:29 +0530

    13-05-2020 18:16 request api

---
+++ python_repos.py
@@ -0,0 +1,20 @@
+import requests
+
+# make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers={'Accept':'application/vnd.github.v3+json'}
+r=requests.get(url,headers=headers)
+print(f"Status code:{r.status_code}")
+#store API response in a variable
+response_dict=r.json()
+print(f"total respositories: {response_dict['total_count']}")
+
+# explore information about the respositories
+repo_dicts=response_dict['items']
+print(f"respositories returned: {len(repo_dicts)}")
+
+# examine the first repository
+repo_dict=repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
